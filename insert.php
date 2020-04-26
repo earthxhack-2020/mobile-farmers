75856dd490b44d44b52a1ddaeeb2a835f3d49c73
dalorion <ldalorion@gmail.com>
2020-04-26 17:06:09 -0500

    fixed farmer submission

--- insert.php
+++ insert.php
@@ -1,42 +1,29 @@
 <html>
-<body>
-	<!--?php
-  ini_set('display_errors', 1);
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
-?-->
-<!--?php
-  ini_set('display_errors', 1);
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
-?-->
+<body>>
 	<?php
-		echo "attempting to add 1 record";
-		echo "name: $_POST[name]";
-		echo "name: $_POST[phone_number]";
-		echo "name: $_POST[email]";
-		echo "name: $_POST[availability]";
-		$con = mysql_connect("us-cdbr-iron-east-01.cleardb.net","bce8ddb0b14438","c656e6d2");
-		if (!$con) {
-			die('Could not connect: ' . mysql_error());
-		}
-		mysql_select_db("heroku_48340ca8bd6e8bd", $con);
-		$sql="INSERT INTO farmer(id, name, phone_number, common_produce, password, email, availability) 
-		       VALUES (NULL,'$_POST[name]','$_POST[phone_number]','$_POST[common_produce]','$_POST[password]','$_POST[email]','$_POST[availability]')";
-		if (!mysql_query($sql,$con)) {
+	    //set PHP variables like this so we can use them anywhere in code below
+	    $u_name = filter_var($_POST["name"], FILTER_SANITIZE_STRING); 
+	    $u_phone_number = filter_var($_POST["phone_number"], FILTER_SANITIZE_STRING); 
+	    $u_common_produce = filter_var($_POST["common_produce"], FILTER_SANITIZE_STRING); 
+	    $u_email = filter_var($_POST["email"], FILTER_SANITIZE_STRING); 
+	    $u_password = filter_var($_POST["password"], FILTER_SANITIZE_STRING); 
+	    $u_availability = filter_var($_POST["availability"], FILTER_SANITIZE_STRING); 
+
+		$mysqli = new mysqli("us-cdbr-iron-east-01.cleardb.net","bce8ddb0b14438","c656e6d2","heroku_48340ca8bd6e8bd");
+
+		if ($mysqli->connect_error) {
+		  die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+	    }
+
+		$statement = $mysqli->prepare("INSERT INTO farmer(id, name, phone_number, common_produce, password, email, availability) VALUES(NULL,?,?,?,?,?,?)"); //prepare sql insert query
+		//bind parameters for markers, where (s = string, i = integer, d = double,  b = blob)
+		$statement->bind_param('ssssss', $u_name, $u_phone_number, $u_common_produce, $u_password, $u_email, $u_availability); //bind values and execute insert query
+		if (!$statement->execute()) {
 			die('Error: ' . mysql_error());
-		}
-		echo "1 record added";
-		mysql_close($con);
-		
-		if(mysql_affected_rows($connect) > 0){
-	      echo "<p>Farmer Added</p>";
-		  echo "<a href=\"home.html\">Go Back</a>";
+			echo "<p><b>Error:  . mysql_error()</b></p>";
 		} else {
-	      echo "Farmer NOT Added<br />";
-		  echo mysqli_error ($connect);
+		  echo "<p>Farmer Added</p>";
+		  echo "<a href=\"home.html\">Go Back</a>";
 		}
 	?>
   </body>
